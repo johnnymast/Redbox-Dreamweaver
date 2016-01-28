4579d205348b938c978819ffe328cdf9747159ea
Johnny Mast <mastjohnny@gmail.com>
2016-01-28 08:29:51 +0100

    Updated examples/decode.php and examples/encode.php to show how the api works.

--- examples/decode.php
+++ examples/decode.php
@@ -1,2 +1,9 @@
 <?php
 require 'autoload.php';
+
+use Redbox\Dreamweaver;
+
+$encoded_password = "53735A3C7A3C7955";
+
+/** Should output. Your decoded password: SrX9v7sN  */
+echo "Your decoded password: ".Dreamweaver\Dreamweaver::decode_password($encoded_password).PHP_EOL;
--- examples/encode.php
+++ examples/encode.php
@@ -1,2 +1,8 @@
 <?php
 require 'autoload.php';
+use Redbox\Dreamweaver;
+
+$password = "SrX9v7sN";
+
+/** Should output. Your encoded password: 53735A3C7A3C7955 */
+echo "Your encoded password: ".Dreamweaver\Dreamweaver::encode_password($password).PHP_EOL;
